ff2563d3f37d8c97ed5dee9d548a0abea8acd746
Thullyoo <thullyofilho@gmail.com>
2025-07-13 18:04:30 -0300

    feat: adjust dateutils test

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -1,7 +1,8 @@
 package br.ce.wcaquino.taskbackend.utils;
 
-import org.junit.Assert;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
@@ -10,18 +11,18 @@ public class DateUtilsTest{
     @Test
     public void shouldReturnTrueForFutureDate(){
         LocalDate date = LocalDate.of(2040, 1, 1);
-        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+        Assertions.assertTrue(DateUtils.isEqualOrFutureDate(date));
     }
 
     @Test
     public void shouldReturnFalseForPastDate(){
         LocalDate date = LocalDate.of(2020, 1, 1);
-        Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+        Assertions.assertFalse(DateUtils.isEqualOrFutureDate(date));
     }
 
     @Test
     public void shouldReturnTrueForPresentDate(){
         LocalDate date = LocalDate.now();
-        Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+        Assertions.assertTrue(DateUtils.isEqualOrFutureDate(date));
     }
 }
